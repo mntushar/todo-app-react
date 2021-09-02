38fe2f0a4471415e60c0c3bf5c96152b38ec678b
mntushar <mntushar25@gmail.com>
2021-09-02 08:40:53 +0600

    final project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import MyRouter from './component/my_router'
-import './cse_component/my_style.css'
 import HeardTitle from '../src/component/heardTitle';
-import './App.scss';
 
 
 export default function App() {
     return (
-    
       <div>
         <HeardTitle />
         <MyRouter />
